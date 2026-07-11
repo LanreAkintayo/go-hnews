3bbc81430c33506531e5b32c3d4645b088387948
LanreAkintayo <akintayolanre2019@gmail.com>
2026-07-11 10:27:52 +0100

    feat: test requireAuth

--- middlewares.go
+++ middlewares.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"log"
 	"net/http"
 )
 
@@ -18,7 +17,7 @@ const (
 
 func (app *application) logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.Path)
+		app.infoLog.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
 }
